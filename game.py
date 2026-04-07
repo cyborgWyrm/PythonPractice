9a90e92164a873232d024cd61e2c0362ae0e73d7
cyborgWyrm <esorRevurg@gmail.com>
2026-04-07 13:34:18 -0600

    made game.py

---
+++ game.py
@@ -0,0 +1,125 @@
+"""
+Ideas:
+fast forward: runs without wait for a set number of times to check if population is stable or goes to zero
+add food, remove food from command line
+reproductive genetics
+more types of feeding genetics
+breeding instead of mitosis
+"""
+
+# imports
+import random
+import time
+
+# blobs are creatures with the ability to eat, reproduce, and die
+# their genetics change how they function
+class Blob:
+    mature: bool = False
+    agression: bool = False
+    food: float = 1
+    fed: bool = False
+
+    def __init__(self,agression):
+        self.agression = agression
+    
+    def feed(self,amount):
+        # blobs will look for food when full, but will not eat
+        #print(self.fed)
+        if self.fed == False:
+            self.food = self.food+amount
+            self.mature = True
+            self.fed = True
+
+    def reproduce(self):
+        return Blob(self.agression)
+
+    def die(self):
+        self = None
+
+# function that controls what happens when two blobs meet to feed
+# an agressive blob meeting a peaceful blob gets 2 food, while the peaceful blob gets none
+# peaceful blobs meeting get 1 food each
+# agressive blobs meeting get 1/2 food each
+def eat(blob0,blob1):
+    if blob0==None:
+        if blob1!=None:
+            blob1.feed(1)
+    elif blob1==None:
+        blob0.feed(1)
+
+    elif blob0.agression==False:
+        if blob1.agression==False:
+            blob0.feed(1)
+            blob1.feed(1)
+        elif blob1.agression==True:
+            blob1.feed(2)
+    
+    elif blob0.agression==True:
+        if blob1.agression==False:
+            blob0.feed(2)
+        elif blob1.agression==True:
+            blob0.feed(0.5)
+            blob1.feed(0.5)
+
+def allFed():
+    for blob in blobs:
+        print(blob.fed)
+        if blob.fed==False:
+            return False
+    return True
+
+def getHungryBlob(notThisOne: Blob):
+    hungryBlobs = []
+    for blob in blobs:
+        if blob.fed==False:
+            hungryBlobs.append(blob)
+    
+    if len(hungryBlobs)<2:
+        return None
+    
+    num = random.randint(0,len(hungryBlobs)-1)
+    if hungryBlobs[num] == notThisOne:
+        return getHungryBlob(notThisOne)
+    return hungryBlobs[num]
+
+blobs = []
+i = 0
+while i<200:
+    num = random.randint(0,1)
+    agressive: bool = False
+    if num==0:
+        agressive=True
+    newBlob = Blob(agressive)
+    blobs.append(newBlob)
+    i=i+1
+
+while len(blobs)>1:
+    foods = 1000
+    for blob in blobs:
+        blob.fed = False
+    while foods>0 and allFed()==False:
+        blob1=getHungryBlob(None)
+        blob2=getHungryBlob(blob1)
+        eat(blob1,blob2)
+        foods = foods-2
+        #print(allFed())
+    index=0
+    for blob in blobs:
+        blob.food = blob.food-1
+        if blob.food<0:
+            blob.die()
+            blobs.pop(index)
+
+        if blob.food>2 and blob.mature:
+            blob.food = blob.food-2
+            blobs.append(blob.reproduce())
+        index=index+1
+    
+    print(f"There are {len(blobs)} blobs")
+    agressiveBlobs=0
+    for blob in blobs:
+        if blob.agression:
+            agressiveBlobs = agressiveBlobs+1
+    print(f"{agressiveBlobs} of them are agressive, {len(blobs)-agressiveBlobs} are not")
+    time.sleep(1)
+
